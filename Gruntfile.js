4d74502e3618743190a1b6547ddfa2117546ee35
Joris Steyn <joris@ibuildings.nl>
2016-07-27 09:39:48 +0200

    Add grunt-watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.initConfig({
         sass: {
@@ -10,6 +11,12 @@ module.exports = function(grunt) {
                 files: {
                     'web/css/main.css': 'app/Resources/css/main.scss'
                 }
+            }
+        },
+        watch: {
+            sass: {
+                files: ['**/*.scss'],
+                tasks: ['sass']
             }
         }
     });
